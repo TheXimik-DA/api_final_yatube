aba5026e5a740704de9cb0c4491c38b7bddd1bfa
Gadzhi Shakhbanov <mister.tool@mail.ru>
2023-03-20 20:54:51 +0300

    second review 9 sprint

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -1,5 +1,4 @@
 from rest_framework import serializers
-from rest_framework.generics import get_object_or_404
 from rest_framework.relations import SlugRelatedField
 from rest_framework.validators import UniqueTogetherValidator
 
